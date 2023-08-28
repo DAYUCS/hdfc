afb29716ddcb0dd5e318c74586f8202da6a30e6e
Dayu Bian <dayub@chinasystems.com>
2023-08-28 16:17:04 +0800

    Dashboard

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
 
 export interface Message {
-  fromName: string;
-  subject: string;
-  date: string;
-  id: number;
-  read: boolean;
+  txType: string;
+  recNumber: number;
 }
 
 @Injectable({
@@ -14,60 +11,24 @@ export interface Message {
 export class DataService {
   public messages: Message[] = [
     {
-      fromName: 'Matt Chorsey',
-      subject: 'New event: Trip to Vegas',
-      date: '9:32 AM',
-      id: 0,
-      read: false
+      txType: 'BG Issuance',
+      recNumber: 9
     },
     {
-      fromName: 'Lauren Ruthford',
-      subject: 'Long time no chat',
-      date: '6:12 AM',
-      id: 1,
-      read: false
+      txType: 'BG Amendment',
+      recNumber: 0
     },
     {
-      fromName: 'Jordan Firth',
-      subject: 'Report Results',
-      date: '4:55 AM',
-      id: 2,
-      read: false
+      txType: 'LC Issuance',
+      recNumber: 3
     },
     {
-      fromName: 'Bill Thomas',
-      subject: 'The situation',
-      date: 'Yesterday',
-      id: 3,
-      read: false
+      txType: 'LC Amendment',
+      recNumber: 0
     },
     {
-      fromName: 'Joanne Pollan',
-      subject: 'Updated invitation: Swim lessons',
-      date: 'Yesterday',
-      id: 4,
-      read: false
-    },
-    {
-      fromName: 'Andrea Cornerston',
-      subject: 'Last minute ask',
-      date: 'Yesterday',
-      id: 5,
-      read: false
-    },
-    {
-      fromName: 'Moe Chamont',
-      subject: 'Family Calendar - Version 1',
-      date: 'Last Week',
-      id: 6,
-      read: false
-    },
-    {
-      fromName: 'Kelly Richardson',
-      subject: 'Placeholder Headhots',
-      date: 'Last Week',
-      id: 7,
-      read: false
+      txType: 'Remittance',
+      recNumber: 2
     }
   ];
 
